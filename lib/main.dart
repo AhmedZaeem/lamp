1e81801ae9a6778c9c86139120fd01bb45010eef
Ahmed <azaeem1@smail.ucas.edu.ps>
2023-07-30 00:13:48 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
             brightness: Brightness.light,
           ),
-          title: 'Quoty',
+          title: 'Lamp',
           home: const Splash(),
         );
       },
